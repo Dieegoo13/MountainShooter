c3f899671ed57ab83bd60d58554f20a795f9450a
Dieegoo13 <diegomiranda1310@gmail.com>
2025-08-13 16:11:37 -0300

    Creating asset folder

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import pygame as  pg
+from code.Game import Game
 
-print('Iniciou o jogo')
-pg.init()
-screen = pg.display.set_mode((800, 600))
-windows = pg.display.set_mode(size=(600, 400))
-print('Encerrou o jogo')
-
-while True:
-    #Check for all events
-    for event in pg.event.get():
-        if event.type == pg.QUIT: #Close Window
-            pg.quit() #end pygame
+game = Game()
+game.run()
